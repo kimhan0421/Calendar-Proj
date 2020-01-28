842b648e6458b83462f34e6f33333ac3eecb21ec
hansol <kimhan0421@naver.com>
2020-01-29 00:05:48 +0900

    Leap year virtual calendar

--- src/honux/calendar/Prompt.java
+++ src/honux/calendar/Prompt.java
@@ -3,24 +3,31 @@ package honux.calendar;
 import java.util.Scanner;
 
 public class Prompt {
-	private final static String PROMPT = "cal>";
+	//private final static String PROMPT = "cal>";
 
 	public void runPrompt() {
 		String PROMPT = "cal>";
 		Scanner sc = new Scanner(System.in);
 		Calendar cal = new Calendar();
-		int month = 1;
+		int month = -1;
+		int year = -1;
+		
 		while (true) {
+			System.out.println("년도를 입력하세요.");
+			System.out.println("YEAR>>");
+			year = sc.nextInt();
 			System.out.println("달을 입력하세요.");
+			System.out.println("MONTH>>");
 			System.out.print(PROMPT);
 			month = sc.nextInt();
+			
 			if (month == -1) {
 				break;
 			}
 			if (month > 12) {
 				continue;
 			}
-			cal.printCalendar(2020,month);
+			cal.printCalendar(year,month);
 		}
 
 		System.out.println("End");
